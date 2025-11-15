6dbb44ccf4ddf6419f63bc61cfcff9b4300e471f
Даниил Антонов <Tyuiu.AntonovDI@outlook.com>
2025-11-15 08:53:40 +0500

    Add project files.

---
+++ Tyuiu.AntonovDI.Sprint5.Task0.V17.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.AntonovDI.Sprint5.Task0.V17.Lib;
+using System.IO;
+
+namespace Tyuiu.AntonovDI.Sprint5.Task0.V17.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\Users\anton\Desktop\Тиу\source\repos\Tyuiu.AntonovDI.Sprint5\Tyuiu.AntonovDI.Sprint5.Task0.V17\bin\Debug\OutPutFileTask0";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExist = fileInfo.Exists; //Проверка сущ-ет ли файл
+            bool wait = false;
+            Assert.AreEqual(wait, fileExist);
+        }
+    }
+}
---
+++ Tyuiu.AntonovDI.Sprint5.Task0.V17/Program.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.AntonovDI.Sprint5.Task0.V17.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        int x = 3;
+        string res = ds.SaveToFileTextData(x);
+        Console.WriteLine("Файл" + res);
+        Console.WriteLine("Создан!");
+    }
+}
