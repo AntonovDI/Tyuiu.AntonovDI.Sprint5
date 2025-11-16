032350f6fa6c4f670e8c7db9faa710facaed5688
Даниил Антонов <Tyuiu.AntonovDI@outlook.com>
2025-11-16 12:42:58 +0500

    Task2

---
+++ Tyuiu.AntonovDI.Sprint5.Task2.V2/Program.cs
@@ -0,0 +1,50 @@
+﻿using Tyuiu.AntonovDI.Sprint5.Task2.V2.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[,] matrix = new int[3, 3] {{-7, 7, 5 },
+                                       { 4, 2, -7},
+                                       { 2, 6, -4}};
+
+        int rows = matrix.GetUpperBound(0) + 1;
+        int columns = matrix.Length / rows;
+
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #5 | Выполнил: Антонов Д. И. | ИСТНб-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #5                                                               *");
+        Console.WriteLine("* Тема: Класс File. Запись структурированных данных в текстовый файл      *");
+        Console.WriteLine("* Задание #2                                                              *");
+        Console.WriteLine("* Вариант #2                                                              *");
+        Console.WriteLine("* Выполнил: Антонов Даниил Иванович | ИСТНб-25-1                          *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Массив: ");
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.Write($"{matrix[i, j]} \t");
+            }
+            Console.WriteLine();
+        }
+        
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        string res = ds.SaveToFileTextData(matrix);
+        Console.WriteLine("Файл: " + res);
+        Console.WriteLine("Создан");
+        Console.ReadKey();
+    }
+}
