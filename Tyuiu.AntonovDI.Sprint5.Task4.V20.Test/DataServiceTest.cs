1a826524e00868e5688d9bdc215c5308bb7369a0
Даниил Антонов <Tyuiu.AntonovDI@outlook.com>
2025-11-27 23:43:06 +0500

    Task4

---
+++ Tyuiu.AntonovDI.Sprint5.Task4.V20.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.AntonovDI.Sprint5.Task4.V20.Lib;
+using System.IO;
+
+namespace Tyuiu.AntonovDI.Sprint5.Task4.V20.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ExistsLoadFromDataFile()
+        {
+            string path = @"C:\DataSprint5\InPutDataFileTask4V20.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.AntonovDI.Sprint5.Task4.V20/Program.cs
@@ -0,0 +1,32 @@
+﻿using Tyuiu.AntonovDI.Sprint5.Task4.V20.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #5 | Выполнил: Антонов Д. И. | ИСТНб-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #5                                                               *");
+        Console.WriteLine("* Тема: Создание итогового решения по спринту                             *");
+        Console.WriteLine("* Задание #4                                                              *");
+        Console.WriteLine("* Вариант #20                                                             *");
+        Console.WriteLine("* Выполнил: Антонов Даниил Иванович | ИСТНб-25-1                          *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        string path = @"C:\DataSprint5\InPutDataFileTask4V20.txt";
+        Console.WriteLine("Файл находится в папке - " + path);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        double res = ds.LoadFromDataFile(path);
+        Console.WriteLine(res);
+        Console.ReadKey();
+    }
+}
