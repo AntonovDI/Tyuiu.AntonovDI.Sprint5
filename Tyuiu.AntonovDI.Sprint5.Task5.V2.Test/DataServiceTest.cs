54d21cd19fe09a264263ac9bb7c59d45c57e7f9a
Даниил Антонов <Tyuiu.AntonovDI@outlook.com>
2025-11-28 14:23:57 +0500

    Task5

---
+++ Tyuiu.AntonovDI.Sprint5.Task5.V2.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using Tyuiu.AntonovDI.Sprint5.Task5.V2.Lib;
+
+
+namespace Tyuiu.AntonovDI.Sprint5.Task5.V2.Test  
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ExistsLoadFromDataFile()
+        {
+            string path = @"C:\DataSprint5\InPutDataFileTask5V2.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(fileExists, wait);
+        }
+    }
+}
---
+++ Tyuiu.AntonovDI.Sprint5.Task5.V2/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.AntonovDI.Sprint5.Task5.V2.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #5 | Выполнил: Антонов Д. И. | ИСТНб-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #5                                                               *");
+        Console.WriteLine("* Тема: Чтение набора данных из текстового файла                          *");
+        Console.WriteLine("* Задание #5                                                              *");
+        Console.WriteLine("* Вариант #2                                                              *");
+        Console.WriteLine("* Выполнил: Антонов Даниил Иванович | ИСТНб-25-1                          *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Дан файл С:|DataSprint5|InPutDataFileTask5V2.txt в котором есть набор   *");
+        Console.WriteLine("* значений. Найти среднее всех положительных значений. Полученный         *");
+        Console.WriteLine("* результат вывести на консоль. У вещественных значений округлить до      *");
+        Console.WriteLine("* трёх знаков после запятой.                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        string path = @"C:\DataSprint5\InPutDataFileTask5V2.txt";
+        Console.WriteLine("Данные находятся в файле: " + path);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        double res = ds.LoadFromDataFile(path);
+        Console.WriteLine("Результат выполнения программы: " + res);
+    }
+}
