c092dbe0dc0c5ac95adf0dc551049e3dfbecac24
Даниил Антонов <Tyuiu.AntonovDI@outlook.com>
2025-11-28 17:15:20 +0500

    Task7

--- Tyuiu.AntonovDI.Sprint5.Task7.V3.Lib/DataService.cs
+++ Tyuiu.AntonovDI.Sprint5.Task7.V3.Lib/DataService.cs
@@ -3,7 +3,7 @@ using tyuiu.cources.programming.interfaces.Sprint5;
 
 namespace Tyuiu.AntonovDI.Sprint5.Task7.V3.Lib
 {
-    public class DataService : ISprint5Task7V9
+    public class DataService : ISprint5Task7V3
     {
         public string LoadDataAndSave(string path)
         {
